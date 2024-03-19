040572400e07fc805e82815947b02612910d0da0
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-03-19 21:30:31 +0100

    Quit button

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -4,7 +4,6 @@ set(IMGUI_SOURCES
     ${Z0_LIBRARIES_DIR}/imgui/imgui_draw.cpp
     ${Z0_LIBRARIES_DIR}/imgui/imgui_widgets.cpp
     ${Z0_LIBRARIES_DIR}/imgui/imgui_tables.cpp
-    ${Z0_LIBRARIES_DIR}/imgui/imgui_demo.cpp
     ${Z0_LIBRARIES_DIR}/imgui/backends/imgui_impl_vulkan.cpp
     ${Z0_LIBRARIES_DIR}/imgui/backends/imgui_impl_glfw.cpp
 )
